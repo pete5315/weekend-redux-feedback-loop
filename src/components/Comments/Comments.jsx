496eef9445e7ac3ac4927318acdb87cd43d8757b
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-06-09 16:39:09 -0500

    Added comments and review components

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,9 +1,46 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+
+
 function Comments() {
 
+  let [comments, setNewComments] = useState({ comments: "" });
+  const dispatch=useDispatch();
+  const history = useHistory()
+
+
+  const handleNewComments = (value, event) => {
+    event.preventDefault();
+    setNewComments({
+      comments: value,
+    });
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch({
+      type: "SEND_FEEDBACK",
+      payload: comments
+    })
+    history.push('/review')
+
+  };
+
   return (
-
-    <div>1</div>
-
+    <div>
+      <h1>Any comments you want to leave?</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Comments? </label>
+        <input
+          onChange={(event) => handleNewComments(event.target.value, event)}
+          type="text"
+          placeholder="name"
+          value={comments.comments}
+        />
+        <button type="submit">Next</button>
+      </form>
+    </div>
   );
 }
 
--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,9 +1,38 @@
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
+
 function Review() {
+  const currentInfo = useSelector((store) => store.currentInfo);
+  const history = useHistory();
+  const handleSubmit = () => {
+    console.log(currentInfo);
+    axios
+      .post("/api/send", {
+        feeling: currentInfo.feeling,
+        understanding: currentInfo.understanding,
+        support: currentInfo.support,
+        comments: currentInfo.comments
+      })
+      .then((response) => {
+        console.log("Client POST success", response);
+      })
+      .catch((error) => {
+        console.log("Error in client side POST", error);
+      });
+    history.push("/form");
+  };
 
   return (
-
-    <div>1</div>
-
+    <div>
+      <h1>Review your feedback</h1>
+      <br></br>
+      <br></br>
+      <p>Feelings: {currentInfo.feeling}</p>
+      <p>Understanding: {currentInfo.understanding}</p>
+      <p>Support: {currentInfo.support}</p>
+      <p>Comments: {currentInfo.comments}</p>
+      <button onClick={handleSubmit}>SUBMIT</button>
+    </div>
   );
 }
 
